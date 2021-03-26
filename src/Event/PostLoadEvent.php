ff79e5e885a60246f343354d9802a513340006a9
Vincent <vincent@les-tilleuls.coop>
2021-03-26 15:46:25 +0100

    Fix CI (#36)

--- src/Event/PostLoadEvent.php
+++ src/Event/PostLoadEvent.php
@@ -18,7 +18,7 @@ use Doctrine\Persistence\Event\LifecycleEventArgs;
 /**
  * @author Vincent Chalamon <vincentchalamon@gmail.com>
  */
-/*final */class PostLoadEvent extends LifecycleEventArgs implements EventInterface
+/*final */ class PostLoadEvent extends LifecycleEventArgs implements EventInterface
 {
     /**
      * {@inheritdoc}
